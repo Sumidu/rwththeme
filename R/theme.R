bf13ff88d4385306111718a05851b86dd4956aef
André Calero Valdez <andrecalerovaldez@gmail.com>
2019-03-15 14:23:14 +0100

    Initial Commit

---
+++ R/theme.R
@@ -0,0 +1,7 @@
+rwththeme <- function(...){
+  theme_bw(base_size=12, base_family="Helvetica Neue", ...) + theme(
+    panel.background  = element_blank(),
+    plot.background = element_blank()
+    #panel.border = element_rect(size=0.75)
+  )
+}
